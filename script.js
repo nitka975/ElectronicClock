2cf6e60b5569d2fa9506218ebdd69e32b10bd56f
nitka975 <nikitalim.kh1@gmail.com>
2024-11-09 18:49:54 +0100

    Output correctly time

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById('clock').textContent = new Date();
+function updateClock(){
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2,'0');
+    const ceconds = String(now.getSeconds()).padStart(2,'0');
+    
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${ceconds}`;
+
+}
+setInterval(updateClock, 1000);
