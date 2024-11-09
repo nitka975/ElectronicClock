bc6f329ff59a63421d4050433063fe77fa8b861e
nitka975 <nikitalim.kh1@gmail.com>
2024-11-09 19:04:30 +0100

    autput timezona NewYork (-5)

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function updateClock(){
 
 }
 setInterval(updateClock, 1000);
+
+function updateClock_timezona() {
+    const now_local = new Date();
+    const select_timezona = -5; //NewYork
+    const hours = String(now_local.getHours()+select_timezona-1).padStart(2, '0');
+    const minutes = String(now_local.getMinutes()).padStart(2,'0');
+    const ceconds = String(now_local.getSeconds()).padStart(2,'0');
+    
+    document.getElementById('timezona').textContent = `${hours}:${minutes}:${ceconds}`;
+}
+
+setInterval(updateClock_timezona, 1000);
