4f5aec9f1f1c9a44b96d3c67335c118d93e5ae14
nitka975 <nikitalim.kh1@gmail.com>
2024-11-16 19:09:12 +0100

    add UTC time

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ function updateClock(){
     const ceconds = String(now.getSeconds()).padStart(2,'0');
     
     document.getElementById('clock').textContent = `${hours}:${minutes}:${ceconds}`;
-
 }
+
 setInterval(updateClock, 1000);
-
-function updateClock_timezona() {
-    const now_local = new Date();
-    const select_timezona = -5; //NewYork
-    const hours = String(now_local.getHours()+select_timezona-1).padStart(2, '0');
-    const minutes = String(now_local.getMinutes()).padStart(2,'0');
-    const ceconds = String(now_local.getSeconds()).padStart(2,'0');
-    
-    document.getElementById('timezona').textContent = `${hours}:${minutes}:${ceconds}`;
-}
-
-setInterval(updateClock_timezona, 100);
